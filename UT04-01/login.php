39dc8602e7cc138e951cf857133092cdf223b16b
matelat-daw <cesarmatelat@gmail.com>
2024-11-25 15:33:25 +0000

    Update 25-11-2024 15:33

--- UT04-01/login.php
+++ UT04-01/login.php
@@ -7,7 +7,7 @@ $phone = $_POST["phone"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bienvenida</title>
 </head>
 <body>
     <?php
