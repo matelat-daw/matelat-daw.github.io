2eee0f6c3a9c9d1a73319ee33d49571733ede576
matelat-daw <cesarmatelat@gmail.com>
2024-11-26 12:05:39 +0000

    Update 26-11-2024 12:05

---
+++ Ejercicio-2/js/script.js
@@ -0,0 +1,35 @@
+function operate()
+{
+    const DOM = {
+        num1: document.getElementById("num1"),
+        num2: document.getElementById("num2"),
+        oper: document.getElementById("oper"),
+        result: document.getElementById("result")
+    };
+    let total;
+    switch (DOM.oper.value)
+    {
+        case "+":
+            total = +DOM.num1.value + +DOM.num2.value;
+            break;
+        case "-":
+            total = DOM.num1.value - DOM.num2.value;
+            break;
+        case "*":
+            total = DOM.num1.value * DOM.num2.value;
+            break;
+        case "/":
+            if (DOM.num2.value == "0")
+            {
+                alert("No Mostroso no podés dividir por 0.");
+                total = "Intenta Otra Operación";
+            }
+            else
+            {
+                console.log(typeof(DOM.num2.value));
+                total = DOM.num1.value / DOM.num2.value;
+            }
+            break;
+    }
+    DOM.result.innerHTML = total;
+}
