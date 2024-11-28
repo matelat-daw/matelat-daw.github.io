01322d46ea6387c924fa72218fb26d3f66f4bf29
Orion68 <orions68@gmail.com>
2024-11-28 07:28:11 +0000

    Update 28-11-2024 7:28

--- Ejercicio-3/js/main.js
+++ Ejercicio-3/js/main.js
@@ -58,3 +58,37 @@
             }
         }
     }
+
+    function verify() // Función para validar las contraseñas de registro de alumnos y las de modificación, también valida el D.N.I.
+    {    
+        let dnielement = document.getElementById("dni");
+        let dni = dnielement.value;
+        var numero, letra, letras;
+        var expresion_regular_dni = /^[XYZ]?\d{1,9}[A-Z]$/;
+    
+        if(expresion_regular_dni.test(dni) === true)
+        {
+            numero = dni.substr(0, dni.length - 1);
+            numero = numero.replace('X', 0);
+            numero = numero.replace('Y', 1);
+            numero = numero.replace('Z', 2);
+            letra = dni.substr(dni.length - 1, 1);
+            numero = numero % 23;
+            letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+            letras = letras.substring(numero, numero + 1);
+            if (letras != letra.toUpperCase())
+            {
+                alert('El D.N.I. o N.I.E. es Incorrecto, Verifica que los Números y la Letra o Letras Estén Bien.');
+                return false; // Si devuelve false no se envía el formulario.
+            }
+            else
+            {
+                return true; // Devuelvo true, envía el formulario.
+            }
+        }
+        else
+        {
+            alert('El D.N.I. o N.I.E. es Incorrecto, Verifica que los Números y la Letra o Letras Estén Bien.');
+            return false; // Si devuelve false no se envía el formulario.
+        }
+    }
